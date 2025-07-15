96bddbd387d3ea252a9a588bc45d0ac9ad268457
Pedro <pedroa.chaves10@gmail.com>
2025-07-15 00:17:35 -0300

    Screen Sound

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿// Screen Sound
+String mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+void ExibirMensagemBoasVindas()
+{
+    //uso do site "Fsymbols" para alterar a fonte 
+    //Verbating Literal = @ "" para mostrar o que será exibido
+    Console.WriteLine(      
+        @"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+        "
+        );
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+    Console.Write("\nDigie sua opção: ");
+    String opcaoEscolhida = Console.ReadLine()!;    //! no final evita que receba um valor nulo
+    int opcaoEscolhidaInt = int.Parse(opcaoEscolhida);
+    switch (opcaoEscolhidaInt) 
+    {
+        case 1: Console.WriteLine("Opção escolhida: " + opcaoEscolhida);
+            break;
+        case 2: Console.WriteLine("Opção escolhida: " + opcaoEscolhida);
+            break;
+        case 3: Console.WriteLine("Opção escolhida: " + opcaoEscolhida);
+            break;
+        case 4: Console.WriteLine("Opção escolhida: " + opcaoEscolhida);
+            break;
+        case -1: Console.WriteLine("Programa encerrado.");
+            break;
+        default: Console.WriteLine("Opção inválida.");
+            break;
+
+
+    }       
+}
+
+
+ExibirMensagemBoasVindas();
+ExibirOpcoesDoMenu();
+
+
+
+
+
+
+
+
+
+// pratica 1 (Concluida)
+/*
+String curso = "cSharp";
+String nome = "Pedro";
+String sobrenome = "Chaves";
+Console.WriteLine("Curso: "+curso);
+Console.WriteLine("Instrutor: "+ nome +" "+ sobrenome);    
+*/
